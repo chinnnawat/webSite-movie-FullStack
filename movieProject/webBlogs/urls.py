e7e67651b124bf524f3b0149fb6eff7e8bd776b3
chinnnawat <chin3896@gmail.com>
2023-09-27 19:33:24 +0700

    add showPlayer

--- movieProject/webBlogs/urls.py
+++ movieProject/webBlogs/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import homePage
+from .views import homePage, showPlayer
 
 urlpatterns = [
-    path('',homePage)
+    path('', homePage),
+    path('player/<int:id>', showPlayer, name='player_detail'),
 ]
--- movieProject/webBlogs/views.py
+++ movieProject/webBlogs/views.py
@@ -49,3 +49,9 @@ def homePage(request):
         blockPerPage2 = paginator2.page(paginator2.num_pages)
     return render(request,"frontEnd/homePage.html",{'categories':categories,'webblogs1':blockPerPage1,'webblogsFilter1':webblogsFilter1,'webblogs6':blockPerPage6,'webblogs2':blockPerPage2})
 
+def showPlayer(request,id):
+    categories = Category.objects.all()
+    # link = webBlogs.objects.filter(category_id=1)
+    singleMovie = webBlogs.objects.get(id=id)
+    return render(request,"frontEnd/showPlay.html",{"singleMovie":singleMovie,'categories':categories})
+
