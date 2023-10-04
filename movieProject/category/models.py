03bcd30538ff863c98a4bcacb853d1f484a65367
chinnnawat <chin3896@gmail.com>
2023-10-04 16:09:29 +0700

    Category Completed

--- movieProject/category/models.py
+++ movieProject/category/models.py
@@ -5,6 +5,6 @@ from django.db import models
 # name ประเภทหนัง
 class Category(models.Model):
     name = models.CharField(max_length=255,unique=True) #unique => True => ห้ามตั้งชื่อประเภทหนังซ้ำกัน 
-
+    
     def __str__(self):
         return self.name
--- movieProject/webBlogs/views.py
+++ movieProject/webBlogs/views.py
@@ -7,6 +7,7 @@ from django.core.paginator import Paginator , EmptyPage , InvalidPage #แบ่
 def homePage(request):
     #Action = 1
     categories = Category.objects.all()
+    loopCategory = Category.objects.all()
     webblogsFilter1 = webBlogs.objects.filter(category_id=1)
     paginator1 = Paginator(webblogsFilter1,4)
 
@@ -53,5 +54,25 @@ def homePage(request):
 
     
     
-    return render(request,"frontEnd/homePage.html",{'categories':categories,'webblogs1':blockPerPage1,'webblogsFilter1':webblogsFilter1,'webblogs6':blockPerPage6,'webblogs2':blockPerPage2,'popular':popular,'suggest':suggest})
+    return render(request,"frontEnd/homePage.html",{'categories':categories,'webblogs1':blockPerPage1,'webblogsFilter1':webblogsFilter1,'webblogs6':blockPerPage6,'webblogs2':blockPerPage2,'popular':popular,'suggest':suggest,'loopCategory':loopCategory})
+
+def searchCategory(request, cat_id):
+    movieList = webBlogs.objects.filter(category_id=cat_id)
+
+    # pagination
+    paginator = Paginator(movieList, 12)  # สร้าง Paginator จาก movieList และกำหนดให้แสดง 3 รายการต่อหน้า
+
+    try:
+        page = int(request.GET.get('page', '1'))
+    except ValueError:
+        page = 1
+
+    try:
+        pagePerwebblog = paginator.page(page)  # ดึงหน้าที่ร้องขอ
+    except (EmptyPage, InvalidPage):
+        pagePerwebblog = paginator.page(paginator.num_pages)  # หากหน้าไม่มีอยู่ให้ใช้หน้าสุดท้าย
+
+    # category
+    categories = Category.objects.all()
+    return render(request, "frontEnd/homePage.html", {'movieList': pagePerwebblog, 'categories': categories})
 
